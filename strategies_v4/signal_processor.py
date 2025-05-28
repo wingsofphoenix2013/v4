c14471ffddc699a6f6a86e48666683842eef272d
Yuriy Biryukov <yuiry@MacBook-Pro-Yuriy-2.local>
2025-05-28 23:14:07 +0300

    fix(signal): добавлен аргумент strategy_registry в run_signal_loop

--- strategies_v4/signal_processor.py
+++ strategies_v4/signal_processor.py
@@ -4,7 +4,7 @@ import asyncio
 import logging
 log = logging.getLogger("SIGNAL_PROCESSOR")
 
-async def run_signal_loop():
+async def run_signal_loop(strategy_registry):
     log.info("🚦 [SIGNAL_PROCESSOR] Запуск цикла обработки сигналов (заглушка)")
     while True:
         await asyncio.sleep(5)
