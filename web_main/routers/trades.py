ad69532f2a7ce28c21e44d0a1164e1e48eeba95a
Yuriy Biryukov <yuiry@MacBook-Pro-Yuriy-2.local>
2025-07-04 10:33:52 +0300

    refactor: разделение BB-таблиц по direction, добавлен фин. баланс, удалены std 1.5 и 1.0

--- web_main/routers/trades.py
+++ web_main/routers/trades.py
@@ -785,6 +785,7 @@ async def strategy_bb_stats(
         "bb_distribution": bb_distribution,
         "bb_pnl_distribution": bb_pnl_distribution,
     })
+
 # 🔸 Статистика стратегии по MFI
 MFI_BINS = [(0, 10), (10, 20), (20, 30), (30, 40), (40, 50),
             (50, 60), (60, 70), (70, 80), (80, 90), (90, float("inf"))]
