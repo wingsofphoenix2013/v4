ebdcc6e92274a29836bac6f880fd9544904b1cb0
Yuriy Biryukov <yuiry@MacBook-Pro-Yuriy-2.local>
2025-09-08 14:56:14 +0300

    chore(strategies_v4): добавлен бэкофилл Redis-счетчиков стратегии (strategy:stats:{sid}) и интеграция в main

--- strategies_v4/strategies_v4_main.py
+++ strategies_v4/strategies_v4_main.py
@@ -12,7 +12,6 @@ from position_opener import run_position_opener_loop
 from position_handler import run_position_handler
 from core_io import run_signal_log_writer, run_position_open_writer, run_position_update_writer
 from strategies_v4_cleaner import run_strategies_v4_cleaner
-from strategies_stats_backfill import run_strategies_stats_backfill
 
 # 🔸 Логгер для главного процесса
 log = logging.getLogger("STRATEGY_MAIN")
@@ -75,7 +74,6 @@ async def main():
         run_safe_loop(run_position_update_writer, "POSITION_UPDATE_WRITER"),
         run_safe_loop(listen_strategy_update_stream, "STRATEGY_STREAM"),
         run_safe_loop(run_strategies_v4_cleaner, "STRATEGY_CLEANER"),
-        run_safe_loop(run_strategies_stats_backfill, "STRAT_STATS_BF"),
     )
     
 # 🔸 Запуск через CLI
